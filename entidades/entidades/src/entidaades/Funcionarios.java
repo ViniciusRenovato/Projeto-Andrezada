f9aec76a2da76cc268e9d1fe1d9db8a96481e4e5
ViniciusRenovato <83253565+ViniciusRenovato@users.noreply.github.com>
2021-04-28 22:24:46 -0400

    entidades pt3

---
+++ entidades/entidades/src/entidaades/Funcionarios.java
@@ -0,0 +1,24 @@
+
+package entidaades;
+
+public class Funcionarios {
+    
+    private String nome;
+    private String funcao;
+    private int idfuncionario;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getFuncao() {
+        return funcao;
+    }
+
+    public int getIdfuncionario() {
+        return idfuncionario;
+    }
+    
+    
+    
+}
---
+++ entidades/entidades/src/main/java/entidaades/Funcionarios.java
@@ -0,0 +1,23 @@
+package entidaades;
+
+public class Funcionarios {
+    
+    private String nome;
+    private String funcao;
+    private int idfuncionario;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getFuncao() {
+        return funcao;
+    }
+
+    public int getIdfuncionario() {
+        return idfuncionario;
+    }
+    
+    
+    
+}
