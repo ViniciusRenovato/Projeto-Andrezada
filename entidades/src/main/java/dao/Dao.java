463329c4fb5dcc4c19d4c97721c31d17fcb4570c
ViniciusRenovato <83253565+ViniciusRenovato@users.noreply.github.com>
2021-05-08 18:01:24 -0400

    dao nada pronta ainda

--- entidades/src/main/java/dao/Dao.java
+++ entidades/src/main/java/dao/Dao.java
@@ -1,14 +1,42 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package dao;
 
-/**
- *
- * @author Matheus
- */
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
 public class Dao {
-    
+    EntityManagerFactory emf = Persistence.createEntityManagerFactory("com.mycompany_entidades_jar_1.0-SNAPSHOTPU");
+    EntityManager em =emf.createEntityManager();
+    EntityTransaction tx =em.getTransaction();
+
+    public void salvar(Object o){
+        tx.begin();
+        em.merge(o);
+        tx.commit();
+    }
+    public void salvarI(Object o){
+        tx.begin();
+        em.persist(o);
+        tx.commit();
+    }
+    public void remove(Object o){
+        tx.begin();
+        em.remove(o);
+        tx.commit();
+    }
+    public List lista(Class c){ 
+        return  em.createQuery("select o from "+c.getSimpleName()+ " o ").getResultList();
+    }
+
+
+    public List listaNative(Class c){ 
+        return  em.createNativeQuery("select * from "+c.getSimpleName(),c).getResultList();
+    }
+     public Object findbyID(Class c,int id){ 
+        return  em.find(c, id);
+    }
+
 }
