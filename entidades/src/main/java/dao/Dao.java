187b3d5a97701a2be108bb1205a05a98f39d35bf
MatheusCampus <Matheus-a-c@hotmail.com>
2021-05-08 17:36:19 -0400

    commit matheus

--- entidades/src/main/java/dao/Dao.java
+++ entidades/src/main/java/dao/Dao.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package entidaades;
+package dao;
 
 /**
  *
  * @author Matheus
  */
-public class Funcionarios {
+public class Dao {
     
 }
--- entidades/src/main/java/entidaades/Servicos.java
+++ entidades/src/main/java/entidaades/Servicos.java
@@ -1,9 +1,78 @@
 
 package entidaades;
 
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 
-public class Servicos {
-    String NomeServico;
-    float valor;
+@Entity
+public class Servicos implements Serializable {
+
+    @OneToMany(mappedBy = "servicos")
+    private List<Agendamento> agendamentos;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    
+    private Integer Id;
+    private String NomeServico;
+    private int IdServico;
+    private float valor;
+    @OneToOne
+    private Funcionario funcionario;    
+
+    public List<Agendamento> getAgendamentos() {
+        return agendamentos;
+    }
+
+    public void setAgendamentos(List<Agendamento> agendamentos) {
+        this.agendamentos = agendamentos;
+    }
+
+    public Integer getId() {
+        return Id;
+    }
+
+    public void setId(Integer Id) {
+        this.Id = Id;
+    }
+
+    public String getNomeServico() {
+        return NomeServico;
+    }
+
+    public void setNomeServico(String NomeServico) {
+        this.NomeServico = NomeServico;
+    }
+
+    public int getIdServico() {
+        return IdServico;
+    }
+
+    public void setIdServico(int IdServico) {
+        this.IdServico = IdServico;
+    }
+
+    public float getValor() {
+        return valor;
+    }
+
+    public void setValor(float valor) {
+        this.valor = valor;
+    }
+
+    public Funcionario getFuncionario() {
+        return funcionario;
+    }
+
+    public void setFuncionario(Funcionario funcionario) {
+        this.funcionario = funcionario;
+    }
+    
+    
     
 }
