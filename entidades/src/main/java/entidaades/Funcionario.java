4c6e74959b79fb0aea41c7241db3f933330e7c64
MatheusCampus <Matheus-a-c@hotmail.com>
2021-05-10 19:36:00 -0400

    envio funcionario

--- entidades/src/main/java/entidaades/Funcionario.java
+++ entidades/src/main/java/entidaades/Funcionario.java
@@ -1,14 +1,66 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package entidaades;
 
-/**
- *
- * @author Matheus
- */
-public class Funcionario {
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Funcionario implements Serializable {
+
+    @OneToOne(mappedBy = "funcionario")
+    private Servicos servicos;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    
+    private Integer Id;
+    private String Funcao;
+    private String NomeFuncionario;
+    private Integer IDFuncionario;
+
+    public Servicos getServicos() {
+        return servicos;
+    }
+
+    public void setServicos(Servicos servicos) {
+        this.servicos = servicos;
+    }
+
+    public Integer getId() {
+        return Id;
+    }
+
+    public void setId(Integer Id) {
+        this.Id = Id;
+    }
+
+    public String getFuncao() {
+        return Funcao;
+    }
+
+    public void setFuncao(String Funcao) {
+        this.Funcao = Funcao;
+    }
+
+    public String getNomeFuncionario() {
+        return NomeFuncionario;
+    }
+
+    public void setNomeFuncionario(String NomeFuncionario) {
+        this.NomeFuncionario = NomeFuncionario;
+    }
+
+    public Integer getIDFuncionario() {
+        return IDFuncionario;
+    }
+
+    public void setIDFuncionario(Integer IDFuncionario) {
+        this.IDFuncionario = IDFuncionario;
+    }
+
     
 }
